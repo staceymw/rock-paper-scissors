ddd853f737a60c4f28688563555f6e09fb083abc
staceymw <116839832+staceymw@users.noreply.github.com>
2022-12-13 15:09:13 -0500

    Clean up code

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,9 +1,3 @@
-/*
-    computer prompts user to choose rock, paper, or scissors.
-    computer randomly generates a rock paper scissors choice.
-    computer must
-*/
-
 function getComputerChoice() {
     const choiceArray = [
         "Rock",
@@ -18,50 +12,44 @@ getComputerChoice();
 function playRound(playerSelection, computerSelection)
 {
     if (playerSelection === "Paper" && computerSelection === "Rock") {
-        return "You win! Paper beats Rock!";
+        return "win";
     } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
-        return "You lose. Scissors beats Paper.";
+        return "lose";
     } else if (playerSelection === "Paper" && computerSelection === "Paper") {
-        return "It's a tie!";
+        return "tie";
     } else if (playerSelection === "Rock" && computerSelection === "Paper") {
-        return "You lose. Paper beats Rock.";
+        return "lose";
     } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
-        return "You win! Rock beats Scissors!"
+        return "win"
     } else if (playerSelection === "Rock" && computerSelection === "Rock"){
-        return "It's a tie!";
+        return "tie";
     } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
-        return "You win! Scissors beats Paper!";
+        return "win";
     }
     else if (playerSelection === "Scissors" && computerSelection === "Rock") {
-        return "You lose. Rock beats Scissors.";
+        return "lose";
     } else {
-        return "It's a tie!";
+        return "tie";
     }
 }
-
-
-//const playerSelection = getPlayerChoice;
-
-
 
 function game() {
     let playerPoints = 0;
     let computerPoints = 0;
 
     for (let i = 0; i < 5; i++) {
-        //get the computer and player selection
-        //use selection vars as args to playRound
-        const computerSelection = getComputerChoice();
         const playerSelection = prompt("Rock, Paper, or Scissors?");
+        const computerSelection = getComputerChoice();
+        //console.log("Your competitor chose " + computerSelection);
         let currentRound = playRound(playerSelection, computerSelection)
-        if (currentRound === "You win! Rock beats scissors!" || currentRound === "You win! Paper beats Rock!" || currentRound === "You win! Scissors beats Paper!") {
+        if (currentRound === "win") {
             playerPoints++;
             console.log("You won this round!");
-        } else if (currentRound === "You lose. Rock beats Scissors." || currentRound === "You lose. Scissors beats Paper." || currentRound === "You lose. Paper beats Rock.") {
+        } else if (currentRound === "lose") {
             computerPoints++;
             console.log("You lost this round.");
         } else {
-            console.log("It's a tie!");
+            console.log("It's a tie");
         }
     } 
     if (playerPoints > computerPoints) {
